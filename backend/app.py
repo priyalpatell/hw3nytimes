849947e0a4014618dcf92faba10a950a7b74a687
pmc14 <pmcpatel@ucdavis.edu>
2025-05-16 11:45:28 -0700

    updated routes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
 import os
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -8,11 +8,57 @@ template_path = os.getenv('TEMPLATE_PATH','templates')
 # Mongo connection
 mongo_uri = os.getenv("MONGO_URI")
 mongo = MongoClient(mongo_uri)
-db = mongo.get_default_database()
+#db = mongo.get_default_database()
+db = mongo['mydatabase']
+collection = db['articles']
+#collection.insert_one({"test":"Testone"})
 
+comments = {
+    "unique_id":"",
+    "comments": [
+    ],
+    "count": 0
+}
+comment = {
+    "user": "",
+    "body": "",
+    "display": True
+}
 
 app = Flask(__name__, static_folder=static_path, template_folder=template_path)
 CORS(app)
+
+@app.route('/post_comments', methods=['POST'])
+def post_article():
+    data = request.get_json()
+    result = collection.insert_one(data)
+    return jsonify({'inserted_id': str(result.inserted_id)})
+
+@app.route('/get_comments', methods=["GET"])
+def get_article():
+    id = request.args.get("id")
+    result = collection.find_one({"test": id})
+    result['_id'] = str(result['_id'])
+    return jsonify(result)
+    
+@app.route('/get_all_comments', methods=['GET'])
+def get_all_articles():
+    result = list(collection.find())
+    for r in result:
+        r['_id'] = str(r['_id'])
+    return jsonify(result)
+
+@app.route('/update_comments', methods=['PUT'])
+def update_article():
+    data = request.get_json()
+    result = collection.update_one(data.id, data.change)
+    return jsonify({"modified_count": result.modified_count})
+
+@app.route('/delete_comments', methods=['DELETE'])
+def delete_article(id):
+    result = collection.delete_one(id)
+    return jsonify({"deleted_count": result.deleted_count})
+
 
 @app.route('/api/key')
 def get_key():
