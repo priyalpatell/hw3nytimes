ea87a2eab6fc9211b4b2cec148dc24d0cac839f0
pmc14 <pmcpatel@ucdavis.edu>
2025-05-18 20:40:01 -0700

    added article routes

---
+++ frontend/src/routes/Test 2.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+	let data = $state();
+    onMount(async () => {
+		try {
+			
+			/*
+			// getting an article
+			let userId = "testval1"
+			const r = await fetch(`http://localhost:8000/get_article?id=${userId}`);
+			data = await r.json();
+			data = data.test;
+			*/
+			// adding an article
+			/*
+			const response = await fetch('http://localhost:8000/post_article', {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({"test":"testval1"})
+			});
+			const temp = await response.json();
+			id_data = temp.inserted_id;
+			*/
+		} catch(error) {
+			console.error('Failed to add/post to db', error);
+		}
+	});
+	let loggedIn = $state(false);
+</script>
+
+<main>
+	<p>{data}</p>
+</main>
